98943f877171b1a46235bec652e0fe34b9be44ce
Ichi <buntonbrondon2010@gmail.com>
2024-10-16 14:23:53 -0400

    Added Basic TestOpCode.java

---
+++ TeamCode/src/main/java/team/phantompanthers/TestOpCode.java
@@ -0,0 +1,17 @@
+package team.phantompanthers;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+// Code for the Operator Mode
+@TeleOp()
+public class TestOpCode extends OpMode {
+
+    public void init() {
+
+    }
+
+    public void loop() {
+
+    }
+}
