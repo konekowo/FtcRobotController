199a6bf30db07c3895970ca8c22edd716ded4bae
Ichi <78321273+BeanZip@users.noreply.github.com>
2024-10-23 14:37:39 -0400

    Added ControlMappings.java

---
+++ TeamCode/src/main/java/team/phantompanthers/ControlMappings.java
@@ -0,0 +1,12 @@
+package team.phantompanthers;
+
+public enum ControlMappings {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+    EXTEND_ARM,
+    RETRACT_ARM,
+    RAISE_ARM,
+    LOWER_ARM
+}
