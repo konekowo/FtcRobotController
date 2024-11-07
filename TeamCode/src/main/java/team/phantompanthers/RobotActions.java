20186d6049b2e8b15a5923f94e79f9babe3b36c9
kone <70107937+konekowo@users.noreply.github.com>
2024-11-07 14:53:39 -0500

    add documentation

--- TeamCode/src/main/java/team/phantompanthers/RobotActions.java
+++ TeamCode/src/main/java/team/phantompanthers/RobotActions.java
@@ -5,25 +5,30 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.jetbrains.annotations.NotNull;
 
 public class RobotActions {
-    public static void raiseArm(double power, long time, @NotNull DcMotor arm, @NotNull DcMotor arm2) {
-        arm.setPower(power);
-        arm2.setPower(power);
+
+    /**
+     * Raise or Lower the arms of the robot.
+     * @param power The speed at which to raise or lower it at. Negative power lowers it, positive power raises it.
+     * @param time The time to raise for.
+     * @param arm The robotic arm to raise or lower.
+     */
+    public static void raiseOrLowerArm(double power, long time, DcMotor arm) {
+        if (arm != null) {
+            arm.setPower(power);
+        }
         if (time > 0) {
             sleep(time);
         }
     }
 
-    public static void lowerArm(double power, long time, @NotNull DcMotor arm, @NotNull DcMotor arm2) {
+    /**
+     * Extend the arm.
+     * @param power The speed at which to extend the arm at. Negative power unextends it, positive power extends it.
+     * @param time The time to extend for.
+     * @param arm The arm to extend.
+     */
+    public static void extendArm(double power, long time, DcMotor arm) {
         arm.setPower(power);
-        arm2.setPower(power);
-        if (time > 0) {
-            sleep(time);
-        }
-    }
-
-    public static void extendArm(double power, long time, @NotNull DcMotor arm, @NotNull DcMotor arm2) {
-        arm.setPower(power);
-        arm2.setPower(power);
         if (time > 0) {
             sleep(time);
         }
@@ -49,6 +54,10 @@ public class RobotActions {
         }
     }
 
+    /**
+     * Stops all motion on all motors.
+     * @param motorSystem The MotorSystem to stop all motors on.
+     */
     public static void stopMotion(MotorSystem motorSystem) {
         motorSystem.setPower("top_left_motor", 0);
         motorSystem.setPower("top_right_motor", 0);
@@ -72,6 +81,10 @@ public class RobotActions {
         }
     }
 
+    /**
+     * Suspends the current thread.
+     * @param milliseconds The time to suspend the thread for.
+     */
     private static void sleep(long milliseconds) {
         try {
             Thread.sleep(milliseconds);
