6ed82736347ba078ec43022a8febfea9a65df5ba
Ichi <78321273+BeanZip@users.noreply.github.com>
2024-11-12 07:33:23 -0500

    Added Doc

--- TeamCode/src/main/java/team/phantompanthers/opcodes/player/PlayerOpCodeBase.kt
+++ TeamCode/src/main/java/team/phantompanthers/opcodes/player/PlayerOpCodeBase.kt
@@ -4,6 +4,10 @@ import com.qualcomm.robotcore.hardware.Gamepad
 import team.phantompanthers.opcodes.OpCodeBase
 
 abstract class PlayerOpCodeBase : OpCodeBase() {
+    /**
+     * Check if there is input from a gamePad.
+     * @return The current controller being used.
+     */
     val currentController = controllerSwitchCheck()
     private fun controllerSwitchCheck(): String {
         return when {
@@ -12,6 +16,11 @@ abstract class PlayerOpCodeBase : OpCodeBase() {
             else -> "No Controller Activity Detected"
         }
     }
+
+    /**
+     * Records Input from any input from the GamePad
+     * @param gamePad The GamePad to check
+     */
     private fun isGamePadActive(gamePad: Gamepad): Boolean {
         return gamePad.a || gamePad.b || gamePad.x || gamePad.y ||
                 gamepad1.dpad_up || gamepad1.dpad_down || gamepad1.dpad_left || gamepad1.dpad_right ||
