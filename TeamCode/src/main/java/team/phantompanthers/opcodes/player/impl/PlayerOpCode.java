70a0893752dd917fc04bd10f1a552bf0b6577ac7
Ichi <78321273+BeanZip@users.noreply.github.com>
2024-11-14 00:06:53 -0500

    Refactored, and updated robotActions.

--- TeamCode/src/main/java/team/phantompanthers/opcodes/player/impl/PlayerOpCode.java
+++ TeamCode/src/main/java/team/phantompanthers/opcodes/player/impl/PlayerOpCode.java
@@ -23,11 +23,10 @@ public class PlayerOpCode extends PlayerOpCodeBase {
         waitForStart();
         resetRuntime();
         while (opModeIsActive()) {
-            RobotActions.drive(motorSystem, ControlMappings.MOVEMENT_X.getFloat(gamepad1),
-                    ControlMappings.MOVEMENT_Y.getFloat(gamepad1), 0);
+            RobotActions.drive(motorSystem, ControlMappings.MOVEMENT_X.getFloat(gamepad1), ControlMappings.MOVEMENT_Y.getFloat(gamepad1), 0);
             RobotActions.driveTurnLeft(motorSystem, ControlMappings.TURN_LEFT.getFloat(gamepad1), 0);
             RobotActions.driveTurnRight(motorSystem, ControlMappings.TURN_RIGHT.getFloat(gamepad1), 0);
-            RobotActions.raiseArm(motorSystem, ControlMappings.RAISE_ARM.getFloat(gamepad1), 0);
+            RobotActions.raiseArm(servoSystem, ControlMappings.RAISE_ARM.getFloat(gamepad1), 0);
             motorSystem.updateMotors();
         }
     }
