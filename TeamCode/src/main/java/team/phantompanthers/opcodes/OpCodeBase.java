86008f7c3d32c91bff8459f8240ef79504644499
Ichi <78321273+BeanZip@users.noreply.github.com>
2024-11-15 22:00:11 -0500

    IDK

--- TeamCode/src/main/java/team/phantompanthers/opcodes/OpCodeBase.java
+++ TeamCode/src/main/java/team/phantompanthers/opcodes/OpCodeBase.java
@@ -15,11 +15,12 @@ public abstract class OpCodeBase extends LinearOpMode {
      */
     protected void initMotorSystem() {
         motorSystem = new MotorSystem(hardwareMap, telemetry);
+        servoSystem = new ServoOBJSystem(hardwareMap, telemetry);
         motorSystem.addMotor("top_left_motor", true);
         motorSystem.addMotor("top_right_motor", true);
         motorSystem.addMotor("bottom_left_motor", false);
         motorSystem.addMotor("bottom_right_motor", true);
-        motorSystem.addMotor("robotic_arm", false);
+        motorSystem.addMotor("robotic_arm",false);
         servoSystem.addMotor("Claw_Vertical");
         servoSystem.addMotor("Claw_Horizontal");
     }
