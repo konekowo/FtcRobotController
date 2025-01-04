9dc991d8ed5ddb2a75ba26777ea6656dbc78484e
kone <70107937+konekowo@users.noreply.github.com>
2025-01-04 05:57:45 -0500

    remove completely wrong documentation (there is no documentation needed for this)

--- TeamCode/src/main/java/team/phantompanthers/ICallback.java
+++ TeamCode/src/main/java/team/phantompanthers/ICallback.java
@@ -1,15 +1,5 @@
 package team.phantompanthers;
 
-/**
- * <h1>ICallback Documentation</h1>
- * Call's a Value and returns a Value
- */
 public interface ICallback {
-    /**
-     * Calls this function with the specified value as its argument and returns the result.
-     *
-     * @param value The argument to pass to the function.
-     * @return The result of the function call.
-     */
     Object call(Object value);
 }
