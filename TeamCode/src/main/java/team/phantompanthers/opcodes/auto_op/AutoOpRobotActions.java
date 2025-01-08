c210dfb8c3bd8b202f6ba4eb6edbc34a35de61fc
kone <70107937+konekowo@users.noreply.github.com>
2025-01-08 11:41:53 -0500

    add methods to rotate the robot by given degrees

--- TeamCode/src/main/java/team/phantompanthers/opcodes/auto_op/AutoOpRobotActions.java
+++ TeamCode/src/main/java/team/phantompanthers/opcodes/auto_op/AutoOpRobotActions.java
@@ -11,6 +11,7 @@ import team.phantompanthers.part_specific.ServoSystem;
 public class AutoOpRobotActions extends RobotActions {
 
     protected static final float DRIVE_METERS_PER_SECOND = 1.5f;
+    protected static final float DRIVE_TURN_ROTATIONS_PER_SECOND = 1f;
 
     public AutoOpRobotActions(MotorSystem motorSystem, ServoSystem servoSystem) {
         super(motorSystem, servoSystem);
@@ -26,15 +27,14 @@ public class AutoOpRobotActions extends RobotActions {
      *
      * @param direction The direction to drive in as a 2D Vector. (values under 1 = drive slower, values are clamped from 0 to 1)
      * @param meters    The amount of meters to drive for.
-     * @return The amount of time in milliseconds it will take to reach the given distance. (This is for pausing the application after this method.)
+     * @return The amount of time in milliseconds it will take for the robot to reach the given distance. (This is for pausing the application after this method.)
      */
     public long drive(Vector2f direction, double meters) {
         direction.setX(MathUtils.clamp(direction.getX(), 0f, 1f));
         direction.setY(MathUtils.clamp(direction.getY(), 0f, 1f));
         super.drive(direction.getX(), direction.getY());
-        long timeMS = Math.round((meters / DRIVE_METERS_PER_SECOND) * 1000);
         this.motorSystem.updateMotors();
-        return timeMS;
+        return Math.round((meters / DRIVE_METERS_PER_SECOND) * 1000);
     }
 
     public void driveTurnLeft(double power) {
@@ -42,9 +42,33 @@ public class AutoOpRobotActions extends RobotActions {
         this.motorSystem.updateMotors();
     }
 
+    /**
+     * Rotate the robot counter clockwise by a given amount of degrees.
+     *
+     * @param degrees The degrees to rotate the robot by.
+     * @return The time in milliseconds it will take to rotate the robot by the given amount of degrees. (This is for pausing the application after this method.)
+     */
+    public long driveTurnLeft(double power, double degrees) {
+        super.driveTurnLeft(power);
+        this.motorSystem.updateMotors();
+        return Math.round(((degrees / 360D) / (DRIVE_TURN_ROTATIONS_PER_SECOND * power)) * 1000);
+    }
+
     public void driveTurnRight(double power) {
         super.driveTurnRight(power);
         this.motorSystem.updateMotors();
+    }
+
+    /**
+     * Rotate the robot clockwise by a given amount of degrees.
+     *
+     * @param degrees The degrees to rotate the robot by.
+     * @return The time in milliseconds it will take to rotate the robot by the given amount of degrees. (This is for pausing the application after this method.)
+     */
+    public long driveTurnRight(double power, double degrees) {
+        super.driveTurnRight(power);
+        this.motorSystem.updateMotors();
+        return Math.round(((degrees / 360D) / (DRIVE_TURN_ROTATIONS_PER_SECOND * power)) * 1000);
     }
 
     public void setClawState(ClawState clawState) {
